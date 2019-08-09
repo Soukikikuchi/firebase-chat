f51cfb947348be5a2e9c2c8ad2593f02abbce0c2
unknown <F20172055@SHORYO.ED.JP>
2019-08-09 15:43:44 +0900

    画像アップロード機能追加

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -2,7 +2,8 @@ firebase.initializeApp({
   apiKey: 'AIzaSyBAO5dgMLvGLmfOL3Y8sIf5eoriCJhxPXM',
   authDomain: 'chat-app.web.app',
   projectId: 'chat-app-fa4a5',
-  storageBucket: ''
+  storageBucket: 'chat-app-fa4a5.appspot.com'
 });
 
 var db = firebase.firestore();
+var firebase = firebase;
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -5,35 +5,10 @@
 
 (function() {
   'use strict';
+  var image_url=""
 
   // ** TODO - firestoreに接続してデータを取得する処理
   console.log("初期処理");
- 
-  // db.collection("chat-app")
-  // .orderBy("date", "desc")
-  
-  // .onSnapshot(function(querySnapshot) {
-  //     querySnapshot.forEach(function(doc) {
-  //         // doc.data() is never undefined for query doc snapshots
-  //         console.log(doc.id, " => ", doc.data());
-          
-          
-  //         $('#comment').append(
-  //           `
-  //           <div class="ui floating message">
-  //             <div class="header"style="margin-bottom:0.4rem;">`+(doc.data().name)+`
-  //             </div>
-  //             <div style="word-wrap: break-word;">
-  //             `+(doc.data().tweet)+`</div >
-  //             `+(doc.data().date)+`
-  //           </div>
-  //           `)
-         
-  //     });
-  // })
-  // .catch(function(error) {
-  //     console.log("Error getting documents: ", error);
-  // });
 
   db.collection('chat-app')
     .orderBy('date', 'desc')
@@ -43,16 +18,34 @@
       $('#comment').empty();
       ss.forEach(doc => {
         const data = doc.data();
-        $('#comment').append(
-          `
-          <div class="ui floating message">
-            <div class="header"style="margin-bottom:0.4rem;">`+(data.name)+`
+
+        if(data.imageUrl){
+          $('#comment').append(
+            `
+            <div class="ui floating message">
+              <div class="header"style="margin-bottom:0.4rem;">`+(data.name)+`
+              </div>
+              <div style="word-wrap: break-word;">
+              `+(data.tweet)+`</div >
+              `+(data.date)+`
+              <img class="ui fluid image" src="`+data.imageUrl+`">
             </div>
-            <div style="word-wrap: break-word;">
-            `+(data.tweet)+`</div >
-            `+(data.date)+`
-          </div>
-          `)
+            `)
+        }else {
+          $('#comment').append(
+            `
+            <div class="ui floating message">
+              <div class="header"style="margin-bottom:0.4rem;">`+(data.name)+`
+              </div>
+              <div style="word-wrap: break-word;">
+              `+(data.tweet)+`</div >
+              `+(data.date)+`
+            </div>
+            `)
+        }
+
+ 
+          //<img class="ui fluid image" src="https://semantic-ui.com/images/wireframe/image.png">
       });
     });
 
@@ -60,9 +53,13 @@
   $('#submit').on('click', function() {
     var name = $('#name').val();
     var tweet = $('#tweet').val();
-   
+
+    var m = moment(); //現在の時刻が入る
+    var output = m.format('YYYY年MM月DD日 HH:mm:ss');
+    console.log(output); // => 2014年10月24日 11:44:00 Friday
     console.log(name);
     console.log(tweet);
+
     if(!name){
       alert(`名前を入れてください`)
       return; 
@@ -76,58 +73,113 @@
       return;
     }
 
-    // $('#comment').append(
-    //   `
-    //   <div class="ui floating message">
-    //     <div class="header"style="margin-bottom:0.4rem;">`+(name)+`
-    //     </div>
-    //     <div style="word-wrap: break-word;">
-    //     `+(tweet)+`</div >
-    //   </div>
-    //   `)
-// Create a root reference
+    var array = {}
 
-      
+    if(image_url !=""){
+      var ref = firebase
+        .storage()
+        .ref()
+        .child('image/' + image_url.name);
+      ref.put(image_url).then(function(snapshot) {
+        firebase
+          .storage()
+          .ref('image/' + image_url.name)
+          .getDownloadURL()
+          .then(url => {
+          
+            console.log(url);
+            array = {
+              name:name,
+              tweet:tweet,   
+              date:output,
+              imageUrl:url
+            }
+            setTeet(array);
+          });
+      });
+  
+    }else {
+      array = {
+        name:name,
+        tweet:tweet,   
+        date:output,
+      }
+      setTeet(array);
+    }
+   
+    // ** TODO - 変数nameと変数hogeをfirestoreにデータを登録する処理
+  });
 
-    var m = moment(); //現在の時刻が入る
-    var output = m.format('YYYY年MM月DD日 HH:mm:ss');
-
-    console.log(output); // => 2014年10月24日 11:44:00 Friday
-
-
-
-      var usersCollectionRef = db.collection('users');
-      db.collection("chat-app").add({
-        name:(name),
-        tweet:(tweet),   
-        date:(output),
-        
-    }).then(function(docRef) {
+  function setTeet(array){
+    db.collection("chat-app").add(array).then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
-       
-    });
-    
       
-    // ** TODO - 変数nameと変数hogeをfirestoreにデータを登録する処理
-  });
+    });
+  }
 
   const checkTweet = function(tweet) {
     // ** TODO - 一言のチェックをする関数
+      
   };
 
   $('#tweet').on('change keyup', function() {
     // ** TODO - フォームに何か入力があったときに、残りの文字数を表示する処理
  
-  
-  
-  
     console.log('一言に何か呟かれています');
     var tweet = $('#tweet').val();
     $("#counter").html(tweet.length+"文字")
-   
-    
   });
+
+  $('#hidden-new-file').on('change', function() {
+    var files = document.getElementById('hidden-new-file').files;
+    var image = files[0];
+
+    if (image == '' || image == null) {
+      return;
+    }
+
+    var type = image.name.split('.');
+    type = type[type.length - 1].toLowerCase();
+
+    if (
+      type == 'jpg' ||
+      type == 'png' ||
+      type == 'gif' ||
+      type == 'jpeg' ||
+      type == 'JPEG' ||
+      type == 'webp'
+    ) {
+      // alert(type);
+    } else {
+      alert('画像は『jpg』『jpeg』『png』『gif』の拡張子から選んで下さい。');
+      return;
+    }
+
+    image_url = image;
+
+    if (!isImage(image_url.name)) {
+      image_url = '';
+      alert('画像が存在していません。もう一度やり直して下さい。');
+      return;
+    }
+
+    $('#set_icon_button').removeClass('disabled');
+    $('#image_url').html(image_url.name);
+  });
+
+  function isImage(picture_url) {
+    var img = new Image();
+    img.src = picture_url;
+    img.onerror = function() {
+      picture_url = 0;
+      return false;
+    };
+    if (picture_url != null && picture_url != '' && picture_url != 0) {
+      return true;
+    }
+    return false;
+  }
 })();
